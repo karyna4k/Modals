2f4b5a2002961c959569f781f824d63f4f4e9bd8
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-11 21:31:52 +0300

    feat: add modal with form

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
+          <!-- first modal -->
           <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
             Show first modal
           </button>
 
-          <!-- first modal -->
           <Modal
             title="First modal"
             v-show="modalFirst"
-            @close="modalFirst = !modalFirst"
+            @close="modalFirst = false"
           >
             <div slot="body">
               <p>
@@ -23,6 +23,30 @@
               <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
                 Well done!
               </button>
+            </div>
+          </Modal>
+
+          <!-- second modal -->
+          <button
+            class="btn btnPrimary"
+            @click="secondModal.show = !secondModal.show"
+          >
+            Show modal with form
+          </button>
+
+          <Modal
+            title="Modal with form"
+            v-show="secondModal.show"
+            @close="secondModal.show = false"
+          >
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input v-model="secondModal.name" required type="text" />
+                <label>Email:</label>
+                <input v-model="secondModal.email" required type="email" />
+                <button class="btn btnPrimary">Submit!</button>
+              </form>
             </div>
           </Modal>
         </div>
@@ -39,7 +63,24 @@ export default {
   data() {
     return {
       modalFirst: false,
+      secondModal: {
+        show: false,
+        name: "",
+        email: "",
+      },
     };
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.secondModal.name,
+        email: this.secondModal.email,
+      });
+
+      this.secondModal.name = "";
+      this.secondModal.email = "";
+      this.secondModal.show = false;
+    },
   },
 };
 </script>
